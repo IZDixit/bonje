0ed925348faec9fa85292154dfab58b7392f5500
IZDixit <dixitimraan@gmail.com>
2024-11-18 16:12:45 +0300

    added vercel.json, adjusted .env and wsgi.py files

--- fuelapp/fuelapp/wsgi.py
+++ fuelapp/fuelapp/wsgi.py
@@ -14,3 +14,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fuelapp.settings")
 
 application = get_wsgi_application()
+
+# We included this to bridge the connection and connect to vercel
+# we had set allowed hosts to be .vercel.app = hence app is important
+app = application
